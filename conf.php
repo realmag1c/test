964e98313f499ca31cb028334cddfe236c1f13f6
magic <magrabota@gmail.com>
2013-07-04 16:13:53 +0300

    first commit

---
+++ conf.php
@@ -0,0 +1,12 @@
+<?
+$config = array(); 
+$config['type'] = 'mysql';
+$config['server'] = "localhost"; 
+$config['login'] ="root";
+$config['passw'] = "creator";
+$config['name_db'] = "new";
+
+$title='Main page';
+//GRANT ALL ON new.* TO user@localhost IDENTIFIED BY 'mGUPpB]!nnJS';
+?>
+
